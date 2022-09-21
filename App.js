bd4aa7be5c73cd60e7a96ac76672e1c6d782052d
Ednardo-Matos <ednardoinacio@outlook.com>
2022-09-20 22:43:04 -0300

    componente cesta

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
   );
 }
 
